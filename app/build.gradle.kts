7d82429c279f36cfbe731ec2ac75b664f9b21b07
darryl05africa-ctrl <darryl05africa@gmail.com>
2026-02-12 17:34:15 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,8 @@ dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
     implementation(composeBom)
 
-    implementation("androidx.core:core-ktx:1.12.0")
+implementation("com.google.android.material:material:1.11.0")
+  implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
 
